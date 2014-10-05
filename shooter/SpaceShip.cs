abdebdc5dde30c3521f003da1b7a54c730b84b12
David <davrodrila@gmail.com>
2014-10-06 01:13:10 +0200

    Made base project, started coding the space logic

---
+++ shooter/SpaceShip.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace shooter
+{
+    class SpaceShip
+    {
+        private Texture2D texture;
+        private float velocidad;
+        private int coordX;
+        private int coordY;
+        private Rectangle hitBox; 
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
+
+        public SpaceShip(int coordX,int coordY){
+            this.coordX = coordX;
+            this.coordY = coordY;
+        }
+
+        public void mover(Sentido sentido)
+        {
+        }
+    }
+
+    enum Sentido
+    {
+        Arriba,
+        Abajo,
+        Derecha,
+        Izquierda
+    }
+}
